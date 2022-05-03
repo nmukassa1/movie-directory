13cb49ae5c3db1e4cdf41daa4310a29f0cc36769
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2022-05-03 10:23:15 +0100

    v

--- script/dist/info.dev.js
+++ script/dist/info.dev.js
@@ -7,27 +7,63 @@ $('document').ready(function () {
   var imgApi = "https://api.themoviedb.org/3/configuration?api_key=".concat(apiKey);
   var mediaType = localStorage.getItem('mediaType');
   var id = localStorage.getItem('id');
-  var apiURL;
+  var apiURL; //VIDEO URL CONDITIONAL
+
+  var ccURL;
   var videoURL;
 
   if (mediaType === 'movie') {
-    apiURL = "https://api.themoviedb.org/3/movie/".concat(id, "?api_key=").concat(apiKey, "&language=en-US");
+    apiURL = "https://api.themoviedb.org/3/movie/".concat(id, "?api_key=").concat(apiKey, "&language=en-UK");
+    ccURL = "https://api.themoviedb.org/3/movie/".concat(id, "/credits?api_key=").concat(apiKey, "&language=en-UK");
+    videoURL = "https://api.themoviedb.org/3/movie/".concat(id, "/videos?api_key=").concat(apiKey, "&language=en-UK");
   } else {
-    apiURL = "https://api.themoviedb.org/3/tv/".concat(id, "?api_key=").concat(apiKey, "&language=en-US");
-  }
-
-  if (mediaType === 'movie') {
-    videoURL = "https://api.themoviedb.org/3/movie/".concat(id, "/videos?api_key=").concat(apiKey, "&language=en-US");
-  } else {
-    videoURL = "https://api.themoviedb.org/3/tv/".concat(id, "/videos?api_key=").concat(apiKey, "&language=en-US");
+    apiURL = "https://api.themoviedb.org/3/tv/".concat(id, "?api_key=").concat(apiKey, "&language=en-UK");
+    ccURL = "https://api.themoviedb.org/3/tv/".concat(id, "/credits?api_key=").concat(apiKey, "&language=en-UK");
+    videoURL = "https://api.themoviedb.org/3/tv/".concat(id, "/videos?api_key=").concat(apiKey, "&language=en-UK");
   }
 
   fetch(apiURL).then(function (res) {
     return res.json();
   }).then(function (item) {
     console.log(item);
-    backdrop(item);
-    videos(videoURL);
+    $('#title').text(item.title || item.name);
+    var releaseDateArr;
+
+    if (item.release_date) {
+      releaseDateArr = item.release_date.split('-');
+    } else {
+      releaseDateArr = item.first_air_date.split('-');
+    }
+
+    var releaseDate = [releaseDateArr[2], releaseDateArr[1], releaseDateArr[0]].join('-');
+    $('#release').text(releaseDate);
+    var genresArr = [];
+    item.genres.forEach(function (gn) {
+      genresArr.push(gn.name);
+    });
+    $('#genre').text(genresArr.join(', '));
+    $('#bio').text(item.overview);
+    var runtime;
+
+    if (item.runtime) {
+      runtime = item.runtime;
+    } else {
+      runtime = item.episode_run_time;
+    }
+
+    var hr;
+    var min = "".concat(runtime % 60, "m");
+
+    if (runtime > 60) {
+      hr = "".concat(Math.floor(runtime / 60), "h");
+    }
+
+    $('#hr').text(hr);
+    $('#min').text(min);
+    backdrop(item); //trailer(videoURL)
+    //CAST & CREW REQUEST
+
+    castCrew(ccURL);
   })["catch"](function (err) {
     return console.log(err);
   });
@@ -36,15 +72,16 @@ $('document').ready(function () {
     fetch(imgApi).then(function (res) {
       return res.json();
     }).then(function (data) {
+      console.log(data);
       var baseURL = data.images.base_url;
       var backdropSize = data.images.backdrop_sizes[1];
       var backdropPath = item.backdrop_path;
       var uniqueURL = "".concat(baseURL).concat(backdropSize).concat(backdropPath);
-      $('img').attr('src', uniqueURL);
+      $('#hero').css('background-image', "url(".concat(uniqueURL, ")"));
     });
   }
 
-  function videos(url) {
+  function trailer(url) {
     fetch(url).then(function (res) {
       return res.json();
     }).then(function (data) {
@@ -63,6 +100,44 @@ $('document').ready(function () {
       var link = "https://www.youtube.com/embed/".concat(key, "?autoplay=1");
       $('iframe').attr('src', link);
       console.log($('iframe'));
+    });
+  }
+
+  function castCrew(castCrewUrl) {
+    fetch(castCrewUrl).then(function (res) {
+      return res.json();
+    }).then(function (data) {
+      console.log(data);
+      var crewArr = data.crew;
+      var director;
+      var directorId;
+
+      for (var i = 0; i < crewArr.length; i++) {
+        if (crewArr[i].job != 'Director') {
+          continue;
+        } else {
+          director = crewArr[i].name;
+          directorId = crewArr[i].id;
+          break;
+        }
+      }
+
+      $('#director-name').text(director);
+      personImg(directorId);
+    });
+  }
+
+  function personImg(id) {
+    fetch("https://api.themoviedb.org/3/person/".concat(id, "/images?api_key=").concat(apiKey)).then(function (res) {
+      return res.json();
+    }).then(function (data) {
+      console.log(data);
+
+      if (data.profiles.length > !1) {
+        $("#director-img").css('background-image', "url(https://image.tmdb.org/t/p/w45".concat(data.profiles[0].file_path, ")"));
+      } else {
+        $('#director-img').text('No image was found');
+      }
     });
   }
 });
--- script/info.js
+++ script/info.js
@@ -9,28 +9,81 @@ $('document').ready(function(){
     const id = localStorage.getItem('id');
     let apiURL;
 
+    //VIDEO URL CONDITIONAL
+
+    let ccURL;
     let videoURL;
 
     if(mediaType === 'movie'){
-        apiURL = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`
+        apiURL = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-UK`
+
+        ccURL = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=en-UK`
+
+        videoURL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=en-UK`
     } else {
-        apiURL = `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`
+        apiURL = `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-UK`
+
+        ccURL = `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${apiKey}&language=en-UK`
+
+        videoURL = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${apiKey}&language=en-UK`
     }
 
-    if(mediaType === 'movie'){
-        videoURL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&language=en-US`
-    } else {
-        videoURL = `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${apiKey}&language=en-US`
-    }
+    
 
     fetch(apiURL)
     .then(res => res.json())
     .then(item => {
         console.log(item)
         
+        $('#title').text(item.title || item.name)
 
+
+
+        let releaseDateArr;
+        if(item.release_date){
+            releaseDateArr = item.release_date.split('-')
+        } else{
+            releaseDateArr = item.first_air_date.split('-')
+        }
+        const releaseDate = [releaseDateArr[2], releaseDateArr[1], releaseDateArr[0]].join('-');
+        $('#release').text(releaseDate)
+
+
+
+
+        let genresArr = [];
+        item.genres.forEach(gn =>{
+            genresArr.push(gn.name)
+        })
+        $('#genre').text(genresArr.join(', '))
+
+        $('#bio').text(item.overview)
+        
+        let runtime;
+        if(item.runtime){
+            runtime = item.runtime
+        } else{
+            runtime = item.episode_run_time
+        }
+        let hr;
+        const min = `${runtime % 60}m`;
+        
+        if(runtime > 60){
+            hr = `${Math.floor(runtime / 60)}h`;
+        }
+        
+        $('#hr').text(hr);
+        $('#min').text(min);
         backdrop(item)
-        videos(videoURL)
+        //trailer(videoURL)
+
+
+
+
+
+
+        //CAST & CREW REQUEST
+        castCrew(ccURL)
         
        
     })
@@ -43,17 +96,17 @@ $('document').ready(function(){
         .then(res => res.json())
         .then(data => {
 
-            
+            console.log(data)
             const baseURL = data.images.base_url;
             const backdropSize = data.images.backdrop_sizes[1];
             const backdropPath = item.backdrop_path;
             const uniqueURL =`${baseURL}${backdropSize}${backdropPath}`;
 
-            $('img').attr('src', uniqueURL)
+            $('#hero').css('background-image', `url(${uniqueURL})`)
         })
     }
 
-    function videos(url){
+    function trailer(url){
         fetch(url)
         .then(res => res.json())
         .then(data =>{
@@ -73,4 +126,46 @@ $('document').ready(function(){
             console.log($('iframe'))
         })
     }
+
+    function castCrew(castCrewUrl){
+        fetch(castCrewUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const crewArr = data.crew;
+
+            let director;
+            let directorId;
+            for(let i = 0; i < crewArr.length; i++){
+                if(crewArr[i].job != 'Director'){
+                    continue
+                } else{
+                    director = crewArr[i].name
+                    directorId = crewArr[i].id
+                    break
+                }
+            }
+            $('#director-name').text(director)
+
+            personImg(directorId)
+
+        })
+    }
+
+    function personImg(id){
+        fetch(`https://api.themoviedb.org/3/person/${id}/images?api_key=${apiKey}`)
+        .then(res => res.json())
+        .then( data => {
+            console.log(data)
+
+            if(data.profiles.length >! 1){
+                $("#director-img").css('background-image', `url(https://image.tmdb.org/t/p/w45${data.profiles[0].file_path})`)
+            } else{
+                $('#director-img').text('No image was found')
+            }
+
+        })
+    }
+
+    
 })
