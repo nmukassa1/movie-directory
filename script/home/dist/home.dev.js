b04e808b85d060e726724e5b172e4e6272be3499
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2022-05-30 17:39:01 +0100

    cleaned up home page js

--- script/home/dist/home.dev.js
+++ script/home/dist/home.dev.js
@@ -1,100 +1,166 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
 $(document).ready(function () {
   //FETCH API
   var year = new Date().getFullYear();
   var apiKey = '335228310c6b751750199c1a453b7347'; //Api to get image details like sizes and logos
 
   var imgApi = "https://api.themoviedb.org/3/configuration?api_key=".concat(apiKey);
-  var discoverMovieApi = "https://api.themoviedb.org/3/discover/movie?api_key=".concat(apiKey, "&page=1&year=").concat(year, "&sort_by=popularity.desc&with_original_language=en");
-  var discoverTvApi = "https://api.themoviedb.org/3/discover/tv?api_key=".concat(apiKey, "&page=1&year=").concat(year, "&sort_by=popularity.desc&with_original_language=en");
   var trendingMovieApi = "https://api.themoviedb.org/3/trending/movie/day?api_key=".concat(apiKey);
   var trendingTvApi = "https://api.themoviedb.org/3/trending/tv/day?api_key=".concat(apiKey);
-  var popularMoviesApi = "https://api.themoviedb.org/3/movie/popular?api_key=".concat(apiKey, "&language=en-US&page=1");
-  var popularTvApi = "https://api.themoviedb.org/3/tv/popular?api_key=".concat(apiKey, "&language=en-US&page=1");
 
-  var Get =
-  /*#__PURE__*/
-  function () {
-    function Get(url, place, mediaType) {
-      _classCallCheck(this, Get);
+  var getData = function getData(url, appendTo) {
+    var startPoint, data, i, posterPath, fetchImgData, imgData, baseUrl, backdropSize, imgSrcLink;
+    return regeneratorRuntime.async(function getData$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return regeneratorRuntime.awrap(fetch(url));
 
-      this.url = url;
-      this.place = place;
-      this.mediaType = mediaType;
-    }
+          case 2:
+            startPoint = _context.sent;
 
-    _createClass(Get, [{
-      key: "getItems",
-      value: function getItems() {
-        var _this = this;
-
-        fetch(this.url).then(function (res) {
-          return res.json();
-        }).then(function (data) {
-          var items = data; //console.log(items)
-
-          fetch(imgApi).then(function (res) {
-            return res.json();
-          }).then(function (data) {
-            //console.log(data)
-            var baseURL = data.images.base_url;
-            var backdropSize = data.images.backdrop_sizes[0]; //const backdropPath = items.results[8].backdrop_path;
-
-            var posterPath; // const uniqueURL =`${baseURL}${backdropSize}${backdropPath}`;
-            //console.log(uniqueURL)
-
-            var mediaType = _this.mediaType;
-            var locationContainer = $("#".concat(_this.place));
-
-            function post() {
-              var loopCount = 10;
-
-              for (var i = 0; i < loopCount; i++) {
-                if (items.results[i].poster_path == null) {
-                  loopCount++;
-                  continue;
-                } else {
-                  posterPath = items.results[i].poster_path;
-                  var uniqueURL = "".concat(baseURL).concat(backdropSize).concat(posterPath);
-                  var element = $("\n                                    <a href=\"page/info.html\" id=\"".concat(items.results[i].id, "\"class=\"item ").concat(mediaType, "\">\n                                        <img class=\"overlay poster\" src=\"").concat(uniqueURL, "\">\n                                    </a>\n                                "));
-                  locationContainer.append(element);
-                }
-              }
+            if (!(startPoint.status === 404)) {
+              _context.next = 5;
+              break;
             }
 
-            post();
-            var poster = document.querySelectorAll('a[href="page/info.html"]');
-            poster.forEach(function (item) {
-              item.addEventListener('click', function () {
-                if (item.classList.contains('movie')) {
-                  localStorage.setItem('mediaType', "movie");
-                } else {
-                  localStorage.setItem('mediaType', "tv");
-                }
+            throw "Page doesn't exist";
 
-                localStorage.setItem('id', "".concat(item.id)); //localStorage.setItem('mediaType', `${item.id}`)
-              });
-            });
-          }); //SECOND FETCH END
-        }) //FIRST GETCH END
-        ["catch"](function (err) {
-          return console.log(err);
-        });
-      } //FUNCTION END
+          case 5:
+            _context.next = 7;
+            return regeneratorRuntime.awrap(startPoint.json());
 
-    }]);
+          case 7:
+            data = _context.sent;
+            i = 0;
 
-    return Get;
-  }();
+          case 9:
+            if (!(i < data.results.length)) {
+              _context.next = 24;
+              break;
+            }
 
-  var movieTrending = new Get(trendingMovieApi, 'trending-movie-container', 'movie');
-  movieTrending.getItems();
-  var tvContainer = new Get(trendingTvApi, 'trending-tv-container', 'tv'); //tvContainer.getItems()
-}); //END
+            posterPath = data.results[i].poster_path; //Fecth img base-url & img-size
+
+            _context.next = 13;
+            return regeneratorRuntime.awrap(fetch(imgApi));
+
+          case 13:
+            fetchImgData = _context.sent;
+            _context.next = 16;
+            return regeneratorRuntime.awrap(fetchImgData.json());
+
+          case 16:
+            imgData = _context.sent;
+            baseUrl = imgData.images.base_url;
+            backdropSize = imgData.images.poster_sizes[4];
+            imgSrcLink = baseUrl + backdropSize + posterPath; //console.log(imgSrcLink)
+
+            $(appendTo).append("\n                <a class=\"poster\" id=\"".concat(data.results[i].id, "\" href=\"page/info.html\"><img src=\"").concat(imgSrcLink, "\"/></a>\n            "));
+
+          case 21:
+            i++;
+            _context.next = 9;
+            break;
+
+          case 24:
+          case "end":
+            return _context.stop();
+        }
+      }
+    });
+  };
+
+  function get() {
+    return regeneratorRuntime.async(function get$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.prev = 0;
+            _context2.next = 3;
+            return regeneratorRuntime.awrap(getData(trendingMovieApi, '.directory'));
+
+          case 3:
+            _context2.next = 8;
+            break;
+
+          case 5:
+            _context2.prev = 5;
+            _context2.t0 = _context2["catch"](0);
+            alert(_context2.t0);
+
+          case 8:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, null, null, [[0, 5]]);
+  }
+
+  get(); // class Get {
+  //     constructor(url, place, mediaType){
+  //         this.url = url;
+  //         this.place = place;
+  //         this.mediaType = mediaType
+  //     }
+  //     getItems(){
+  //         fetch(this.url)
+  //         .then(res => res.json())
+  //         .then(data => {
+  //             const items = data;
+  //             //console.log(items)
+  //             fetch(imgApi)
+  //             .then(res => res.json())
+  //             .then(data => {
+  //                 //console.log(data)
+  //                 const baseURL = data.images.base_url;
+  //                 const backdropSize = data.images.backdrop_sizes[0];
+  //                 //const backdropPath = items.results[8].backdrop_path;
+  //                 let posterPath;
+  //                 // const uniqueURL =`${baseURL}${backdropSize}${backdropPath}`;
+  //                 //console.log(uniqueURL)
+  //                 const mediaType = this.mediaType;
+  //                 const locationContainer = $(`.${this.place}`);
+  //                 function post(){
+  //                     let loopCount = 10;
+  //                     for(let i = 0; i < loopCount; i++){
+  //                         if(items.results[i].poster_path == null){
+  //                             loopCount++
+  //                             continue
+  //                         } else{
+  //                             posterPath = items.results[i].poster_path;
+  //                             const uniqueURL =`${baseURL}${backdropSize}${posterPath}`;
+  //                             let element = $(`
+  //                                 <a href="page/info.html" id="${items.results[i].id}"class="item ${mediaType}">
+  //                                     <img class="overlay poster" src="${uniqueURL}">
+  //                                 </a>
+  //                             `);
+  //                             locationContainer.append(element)
+  //                         }
+  //                     }
+  //                 }
+  //                 post()
+  //                 const poster = document.querySelectorAll('a[href="page/info.html"]');
+  //                 poster.forEach(item => {
+  //                     item.addEventListener('click', function(){
+  //                         if(item.classList.contains('movie')){
+  //                             localStorage.setItem('mediaType', `movie`)
+  //                         } else{
+  //                             localStorage.setItem('mediaType', `tv`)
+  //                         }
+  //                         localStorage.setItem('id', `${item.id}`)
+  //                         //localStorage.setItem('mediaType', `${item.id}`)
+  //                     })
+  //                 })
+  //             }) //SECOND FETCH END
+  //         }) //FIRST GETCH END
+  //         .catch(err => console.log(err))
+  //     } //FUNCTION END
+  // }
+  // const movieTrending = new Get(trendingMovieApi, 'directory', 'movie');
+  // movieTrending.getItems()
+  // const tvContainer = new Get(trendingTvApi, 'trending-tv-container', 'tv');
+  //tvContainer.getItems()
+});
--- script/home/home.js
+++ script/home/home.js
@@ -6,101 +6,130 @@ $(document).ready(function(){
     const apiKey = '335228310c6b751750199c1a453b7347';
     //Api to get image details like sizes and logos
     const imgApi = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
-    const discoverMovieApi = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&page=1&year=${year}&sort_by=popularity.desc&with_original_language=en`;
-    const discoverTvApi = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&page=1&year=${year}&sort_by=popularity.desc&with_original_language=en`;
 
     const trendingMovieApi = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
     const trendingTvApi = `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}`;
 
-    const popularMoviesApi = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-    const popularTvApi = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`
+    const getData = async (url, appendTo) =>{
+        const startPoint = await fetch(url);   
+        //HANDLE 404 ERRORS
+        if(startPoint.status === 404) throw `Page doesn't exist`
 
-    class Get {
-        constructor(url, place, mediaType){
-            this.url = url;
-            this.place = place;
-            this.mediaType = mediaType
+        const data = await startPoint.json();
+        //console.log(data)
+
+        for(let i = 0; i < data.results.length; i++){
+            const posterPath = data.results[i].poster_path;
+        
+            //Fecth img base-url & img-size
+            const fetchImgData = await fetch(imgApi);
+            const imgData = await fetchImgData.json();
+
+            const baseUrl = imgData.images.base_url;
+            const backdropSize = imgData.images.poster_sizes[4];
+
+            const imgSrcLink = baseUrl + backdropSize + posterPath;
+            //console.log(imgSrcLink)
+
+            $(appendTo).append(`
+                <a class="poster" id="${data.results[i].id}" href="page/info.html"><img src="${imgSrcLink}"/></a>
+            `)
         }
-        getItems(){
-            fetch(this.url)
-            .then(res => res.json())
-            .then(data => {
-                const items = data;
-                //console.log(items)
-                
-
-                fetch(imgApi)
-                .then(res => res.json())
-                .then(data => {
-                    //console.log(data)
-                    const baseURL = data.images.base_url;
-                    const backdropSize = data.images.backdrop_sizes[0];
-                    //const backdropPath = items.results[8].backdrop_path;
-                    let posterPath;
-
-                    // const uniqueURL =`${baseURL}${backdropSize}${backdropPath}`;
-                    //console.log(uniqueURL)
-
-                    const mediaType = this.mediaType;
-                    const locationContainer = $(`#${this.place}`);
-                    function post(){
-                        let loopCount = 10;
-                        for(let i = 0; i < loopCount; i++){
-                            if(items.results[i].poster_path == null){
-                                loopCount++
-                                continue
-
-                            } else{
-                                posterPath = items.results[i].poster_path;
-                                const uniqueURL =`${baseURL}${backdropSize}${posterPath}`;
-        
-                                let element = $(`
-                                    <a href="page/info.html" id="${items.results[i].id}"class="item ${mediaType}">
-                                        <img class="overlay poster" src="${uniqueURL}">
-                                    </a>
-                                `);
-        
-                                locationContainer.append(element)
-                            }
-                        }
-                    }
-
-                    post()
-
-                    
-                    const poster = document.querySelectorAll('a[href="page/info.html"]');
-                    poster.forEach(item => {
-                        item.addEventListener('click', function(){
-                            if(item.classList.contains('movie')){
-                                localStorage.setItem('mediaType', `movie`)
-                            } else{
-                                localStorage.setItem('mediaType', `tv`)
-                            }
-                            localStorage.setItem('id', `${item.id}`)
-                            //localStorage.setItem('mediaType', `${item.id}`)
-                        })
-                    })
-                
-                }) //SECOND FETCH END
-            }) //FIRST GETCH END
-            .catch(err => console.log(err))
-        } //FUNCTION END
-
-
+            
     }
 
-    const movieTrending = new Get(trendingMovieApi, 'trending-movie-container', 'movie');
-    movieTrending.getItems()
+    async function get(){
+        try{
+            await getData(trendingMovieApi, '.directory')
+        } catch (e){
+            alert(e)
+        }
+    }
+
+    get()
+
+    // class Get {
+    //     constructor(url, place, mediaType){
+    //         this.url = url;
+    //         this.place = place;
+    //         this.mediaType = mediaType
+    //     }
+    //     getItems(){
+    //         fetch(this.url)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const items = data;
+    //             //console.log(items)
+                
+
+    //             fetch(imgApi)
+    //             .then(res => res.json())
+    //             .then(data => {
+    //                 //console.log(data)
+    //                 const baseURL = data.images.base_url;
+    //                 const backdropSize = data.images.backdrop_sizes[0];
+    //                 //const backdropPath = items.results[8].backdrop_path;
+    //                 let posterPath;
+
+    //                 // const uniqueURL =`${baseURL}${backdropSize}${backdropPath}`;
+    //                 //console.log(uniqueURL)
+
+    //                 const mediaType = this.mediaType;
+    //                 const locationContainer = $(`.${this.place}`);
+    //                 function post(){
+    //                     let loopCount = 10;
+    //                     for(let i = 0; i < loopCount; i++){
+    //                         if(items.results[i].poster_path == null){
+    //                             loopCount++
+    //                             continue
+
+    //                         } else{
+    //                             posterPath = items.results[i].poster_path;
+    //                             const uniqueURL =`${baseURL}${backdropSize}${posterPath}`;
+        
+    //                             let element = $(`
+    //                                 <a href="page/info.html" id="${items.results[i].id}"class="item ${mediaType}">
+    //                                     <img class="overlay poster" src="${uniqueURL}">
+    //                                 </a>
+    //                             `);
+        
+    //                             locationContainer.append(element)
+    //                         }
+    //                     }
+    //                 }
+
+    //                 post()
+
+                    
+    //                 const poster = document.querySelectorAll('a[href="page/info.html"]');
+    //                 poster.forEach(item => {
+    //                     item.addEventListener('click', function(){
+    //                         if(item.classList.contains('movie')){
+    //                             localStorage.setItem('mediaType', `movie`)
+    //                         } else{
+    //                             localStorage.setItem('mediaType', `tv`)
+    //                         }
+    //                         localStorage.setItem('id', `${item.id}`)
+    //                         //localStorage.setItem('mediaType', `${item.id}`)
+    //                     })
+    //                 })
+                
+    //             }) //SECOND FETCH END
+    //         }) //FIRST GETCH END
+    //         .catch(err => console.log(err))
+    //     } //FUNCTION END
+
+
+    // }
+    
+    // const movieTrending = new Get(trendingMovieApi, 'directory', 'movie');
+    // movieTrending.getItems()
+    
 
     
     
 
-    const tvContainer = new Get(trendingTvApi, 'trending-tv-container', 'tv');
+    // const tvContainer = new Get(trendingTvApi, 'trending-tv-container', 'tv');
     //tvContainer.getItems()
 
-    
-
-
-
-
-}) //END
+})
